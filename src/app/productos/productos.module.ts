599737250bdebd2894a3b3fcae1ea5d3dab9bfa0
Theo wald <theo83wald@gmail.com>
2021-06-10 15:26:28 +0200

    primer form

---
+++ src/app/productos/productos.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductosRoutingModule } from './productos-routing.module';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AgregarComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductosRoutingModule,
+    ReactiveFormsModule
+  ]
+})
+export class ProductosModule { }
